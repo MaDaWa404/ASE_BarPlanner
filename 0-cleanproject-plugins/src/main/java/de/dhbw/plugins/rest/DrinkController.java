ca85868dc0911d55d6caf96bd02a6effedf83306
MaDaWa404 <marcel.david.wagner@gmail.com>
2023-04-04 16:39:58 +0200

    added query parameters for get request

--- 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/rest/DrinkController.java
+++ 0-cleanproject-plugins/src/main/java/de/dhbw/plugins/rest/DrinkController.java
@@ -6,10 +6,7 @@ import de.dhbw.cleanproject.adapter.drink.DrinkToDrinkResourceMapper;
 import de.dhbw.cleanproject.domain.drink.Drink;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
@@ -30,7 +27,12 @@ public class DrinkController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<DrinkResource> getDrinks() {
+    public List<DrinkResource> getDrinks(@RequestParam(required = false) String title) {
+        if(title!=null) {
+            return this.drinkApplicationService.findDrinksWithTitle(title).stream()
+                    .map(drinkToDrinkResourceMapper)
+                    .collect(Collectors.toList());
+        }
         return this.drinkApplicationService.findAllDrinks().stream()
                 .map(drinkToDrinkResourceMapper)
                 .collect(Collectors.toList());
